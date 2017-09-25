3d01df058ba6e04741de2b2e2d0fd9c68646d5f2
Dale Lim <dtlim@voyagerinnovation.com>
2017-09-25 22:53:56 +0800

    Initial commit.

---
+++ app/src/main/java/com/dtlim/recyclerviewanimations/FeedAdapter.java
@@ -0,0 +1,25 @@
+package com.dtlim.recyclerviewanimations;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by dale on 9/24/2017.
+ */
+
+public class FeedAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
